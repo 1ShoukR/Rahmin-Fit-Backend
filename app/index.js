766f51d33f193f3a76b0b81f4e4bda33b40a4cc9
1ShoukR <rahminshoukoohi@gmail.com>
2022-11-28 22:27:48 -0500

    inital express setup. Add Sequelize next

--- app/index.js
+++ app/index.js
@@ -4,3 +4,22 @@ const app = express();
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const PORT = process.env.PORT || 3004;
+
+
+// add sequelize to this backend
+
+
+// Middleware
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, console.log(`Listening on port ${PORT}`));
